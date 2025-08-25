1445bdf111acbaafd0261b0cbef8328315e56c74
khaitbek <khaitbek2005@gmail.com>
2025-08-25 07:56:04 +0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # Zen browser flake from https://github.com/0xc000022070/zen-browser-flake
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+    };
+
+    nil = {
+      url = "github:oxalica/nil#";
+    };
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+  };
+
+  outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./nixos/configuration.nix
+          {
+            environment.systemPackages = with pkgs; [
+              zen-browser.packages.${system}.twilight
+            ];
+          }
+	  home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.hayitbek = import ./home-manager/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
+    };
+
+}
